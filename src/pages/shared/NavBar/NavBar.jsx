176e40d02a927360c3d2d121524cf894c4db88a9
touhidcodes <touhidcodes@gmail.com>
2023-07-28 21:59:31 +0600

    navbar

--- src/pages/shared/NavBar/NavBar.jsx
+++ src/pages/shared/NavBar/NavBar.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 
 const NavBar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <a>Learner</a>
+      </li>
+      <li>
+        <a>Talent</a>
+      </li>
+      <li>
+        <a>Company</a>
+      </li>
+    </>
+  );
+
   return (
-    <div className="navbar bg-blue-500 rounded-xl">
+    <div className="navbar bg-white rounded-xl px-10">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -23,54 +37,21 @@ const NavBar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-xl"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {navOptions}
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case font-bold text-2xl">
+          Employee Pro
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1 text-xl">{navOptions}</ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+      <div className="navbar-end space-x-3">
+        <button className="btn btn-outline btn-info px-7">Log In</button>
+        <button className="btn btn-info px-7 text-white">Join</button>
       </div>
     </div>
   );
