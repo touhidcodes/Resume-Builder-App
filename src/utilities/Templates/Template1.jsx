29cfbdd1f91d9840c518b06fdce186e572de3258
touhidcodes <touhidcodes@gmail.com>
2023-08-02 00:33:24 +0600

    data recieve

--- src/utilities/Templates/Template1.jsx
+++ src/utilities/Templates/Template1.jsx
@@ -18,6 +18,9 @@ const Template1 = (props) => {
       <div className="bg-blue-500 text-white">
         Your Name: {getContactData("name")}
       </div>
+      <div className="bg-blue-500 text-white">
+        Your Name: {getContactData("email")}
+      </div>
     </div>
   );
 };
