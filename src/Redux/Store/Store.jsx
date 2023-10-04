46b29eefa913ed3954b6807cd3de31d316cae607
touhidcodes <touhidcodes@gmail.com>
2023-10-04 22:44:32 +0600

    redux persistor init

--- src/Redux/Store/Store.jsx
+++ src/Redux/Store/Store.jsx
@@ -1,5 +1,16 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import rootReducer from "../Reducers/Reducers";
 
-export const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, compose(applyMiddleware(thunk)));
+const persistor = persistStore(store);
+export { store, persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import "./index.css";
 import router from "./routes/Router/router.jsx";
 import { RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./Redux/Store/Store";
+import { store, persistor } from "./Redux/Store/Store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <div className="mx-auto max-w-screen-xl">
-        <RouterProvider router={router} />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="mx-auto max-w-screen-xl">
+          <RouterProvider router={router} />
+        </div>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
