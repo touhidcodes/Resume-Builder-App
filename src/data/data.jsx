a0980d4514a1d5a4030edfe4b80c9a42dd8146ea
touhidcodes <touhidcodes@gmail.com>
2023-10-12 21:24:26 +0600

    react pdf init

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,5 +1,7 @@
 import Template1 from "../utilities/Templates/Template1";
 import Template2 from "../utilities/Templates/Template2";
+import Template3 from "../utilities/Templates/Template3";
+import Template4 from "../utilities/Templates/Template4";
 import images from "./imagesData";
 
 // Stores All Templates As a json State
@@ -22,14 +24,14 @@ const templates = [
     id: 3,
     type: "local",
     thumbnail: images.image3,
-    data: <Template1 />,
+    data: <Template3 />,
   },
 
   {
     id: 4,
     type: "local",
     thumbnail: images.image4,
-    data: <Template2 />,
+    data: <Template4 />,
   },
 ];
 
---
+++ src/utilities/Templates/Template4.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+import { PDFViewer } from "@react-pdf/renderer";
+
+const Template4 = () => {
+  // Create styles
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: "row",
+      backgroundColor: "#E4E4E4",
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+  });
+
+  return (
+    <div>
+      <PDFViewer className="h-[700px] w-[550px]" showToolbar={false}>
+        <Document>
+          <Page size="A4" style={styles.page}>
+            <View style={styles.section}>
+              <Text>Section #1</Text>
+            </View>
+            <View style={styles.section}>
+              <Text>Section #2</Text>
+            </View>
+          </Page>
+        </Document>
+      </PDFViewer>
+    </div>
+  );
+};
+
+export default Template4;
