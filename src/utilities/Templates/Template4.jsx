9666361d1dda4a34b35e4f9d71ee4374b6de59d1
touhidcodes <touhidcodes@gmail.com>
2023-10-17 23:45:18 +0600

    x

--- src/utilities/Templates/Template4.jsx
+++ src/utilities/Templates/Template4.jsx
@@ -46,7 +46,6 @@ const Template4 = () => {
           <Page size="A4" style={styles.page}>
             <View style={styles.section}>
               <Text>Section #1</Text>
-              <Text>Section #1</Text>
             </View>
             <View style={styles.section}>
               <Text>Section #2</Text>
